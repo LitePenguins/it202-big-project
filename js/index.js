b17fc7d6305f9ec4bfcb4ae2969efb962fce1ad6
John <makutarulz4@gmail.com>
2020-05-03 19:19:50 -0500

    ghetto saved locations

--- js/index.js
+++ js/index.js
@@ -42,7 +42,8 @@ document.querySelectorAll('.mdc-tab').forEach(item => {
 const tabBar = document.querySelector('.mdc-tab-bar').MDCTabBar;
 window.mdc.autoInit();
 
-const endPoint = "https://api.openchargemap.io/v3/poi/?key=90b0a208-e066-4755-83fa-c6f75fd9c7ad&output=json&maxresults=50&compact=true&verbose=false&latitude=41.983430&longitude=-87.787240&distance=50";
+
+let center = {};
 var map;
 
 // Attach your callback function to the `window` object
@@ -72,7 +73,7 @@ function initMap() {
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
-      let pos = {
+       center = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
@@ -80,7 +81,9 @@ function initMap() {
       // infoWindow.setPosition(pos);
       // infoWindow.setContent('Location found.');
       // infoWindow.open(map);
-      map.setCenter(pos);
+      console.log(center);
+      populateMap();
+      map.setCenter(center);
     });
   } else {
     // Browser doesn't support Geolocation
@@ -88,26 +91,99 @@ function initMap() {
   }
 }
 
-fetch(endPoint).then((res) => {
-  return res.json();
-}).then((json) => {
-  console.log(json);
+let elements = [];
+let clickedLat = '';
+let clickedLong = '';
+let savedLocations = [];
 
-  json.forEach(element => {
+function populateMap() {
+  let jsonData = "";
 
-    let title = element.AddressInfo.Title;
-    //console.log(title);
-    //if (title.includes("Supercharger")) {
-    //console.log(element);
-    let location = {
-      lat: element.AddressInfo.Latitude,
-      lng: element.AddressInfo.Longitude
+  console.log(center);
+  let lat = center.lat;
+  let long = center.lng;
+  //console.log("lat: " + lat + "long: " + long);
+  const endPoint = "https://api.openchargemap.io/v3/poi/?key=90b0a208-e066-4755-83fa-c6f75fd9c7ad&output=json&maxresults=25&compact=true&verbose=false&latitude=" +
+  lat + "&longitude=" +
+  long + "&distance=50";
+
+  fetch(endPoint).then((res) => {
+    return res.json();
+  }).then((json) => {
+    console.log(json);
+
+    let infowindow = new google.maps.InfoWindow();
+
+    json.forEach(element => {
+      console.log(element);
+
+      //console.log(title);
+      let title = element.AddressInfo.Title;
+      let address1 = element.AddressInfo.AddressLine1;
+      let town = element.AddressInfo.Town;
+      let state = element.AddressInfo.StateOrProvince;
+      let postCode = element.AddressInfo.Postcode;
+
+      let contentString = '<div id="content">' +
+        '<h3 id="chargerTitle">' + title + '</h3>' +
+        '<p id="address">' + address1 + '</p>' +
+        '<p id="address2">' + town + ", " + state + " " + postCode + '</p>' +
+        '<button class="mdc-button mdc-button--raised saveButton">  <span class="mdc-button__ripple saveButton"></span> Save Location</button>' +
+        '</div>';
+
+      let location = {
+        lat: element.AddressInfo.Latitude,
+        lng: element.AddressInfo.Longitude
+      };
+
+      //if (title.includes("Supercharger")) {
+      let marker = new google.maps.Marker({
+        position: location,
+        map: map
+      });
+
+      marker.addListener('click', () => {
+        infowindow.setContent(contentString);
+        infowindow.open(map, marker);
+        console.log("lat: " + marker.getPosition().lat());
+        clickedLat = marker.getPosition().lat();
+        console.log("lat: " + marker.getPosition().lng());
+        clickedLong = marker.getPosition().lng();
+      });
+      //}
+    });
+  });
+  //console.log(elements);
+}
+
+document.addEventListener('click', (event) => {
+  //console.log("klick");
+  let element = event.target;
+  //console.log(element);
+  if(element.classList.contains("saveButton")){
+    console.log(clickedLat);
+    console.log(clickedLong);
+
+    let clickedLocation = {
+      lat: clickedLat,
+      lng: clickedLong
     };
 
-    let marker = new google.maps.Marker({
-      position: location,
-      map: map
-    });
-    //}
-  });
+    savedLocations.push(clickedLocation);
+    populateSavedList();
+  }
 });
+
+function populateSavedList() {
+  document.querySelector('#savedTab').innerHTML = '';
+  
+  savedLocations.forEach(element => {
+    let p1 = document.createElement("p");
+      p1.textContent = element.lat;
+      document.querySelector('#savedTab').append(p1);
+
+      let p2 = document.createElement("p");
+      p2.textContent = element.lng;
+      document.querySelector('#savedTab').append(p2);
+  });
+}
